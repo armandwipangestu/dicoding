0157ac188f2c5da852303e9d9cdcea23ff7d50c1
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-06-14 21:29:49 +0700

    JavaScript: Tipe Data -> String

---
+++ back-end/Belajar Dasar Pemrograman JavaScript/Tipe Data/string.js
@@ -0,0 +1,17 @@
+/*
+Strings:
+	Tipe data selanjutnya adalah string yang merupakan sebuah teks. Untuk menetapkan nilai
+	sebagai string pada variable gunakan tanda petik satu ('') atau petik dua ("") di antara
+	teksnya. Contohnya:
+*/
+
+let greet = "Hello";
+console.log(typeof greet); // Output: string
+
+/*
+	Tidak ada perbedaan antara menggunakan petik satu atau petik dua. Anda dapat menggunakan
+	tanda petik secara bergantian, khususnya jika anda memiliki teks yang mengandung tanda petik.
+*/
+
+const question = '"What do you think of JavaScript?" I asked';
+console.log(question); // Output: "What do you think of JavaScript? I asked"
