195e9e97c7d8d190c89691d17eb991615fb3ca79
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-06-14 20:51:24 +0700

    JavaScript: Tipe Data -> Aritmatika

---
+++ back-end/Belajar Dasar Pemrograman JavaScript/Tipe Data/aritmatika.js
@@ -0,0 +1,9 @@
+let a = 12;
+let b = 9;
+
+console.log(`${a} + ${b}: ${a + b}`); // Penjumlahan
+console.log(`${a} - ${b}: ${a - b}`); // Pengurangan
+console.log(`${a} * ${b}: ${a * b}`); // Perkalian
+console.log(`${a} / ${b}: ${a / b}`); // Pembagian
+console.log(`${a} % ${b}: ${a % b}`); // Sisa Bagi
+console.log(`${a} ** ${b}: ${a ** b}`); // Perpangkatan
