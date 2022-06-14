1d95dc09ba456dda57781f510990969d0dda4a13
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-06-14 21:23:55 +0700

    JavaScript: Tipe Data -> BigInt

--- back-end/Belajar Dasar Pemrograman JavaScript/Tipe Data/BigInt.js
+++ back-end/Belajar Dasar Pemrograman JavaScript/Tipe Data/BigInt.js
@@ -13,10 +13,30 @@ BigInt:
 const bigNumber = 1234567890123456789012345678901234567890n;
 const myInt = 1234567890123456789012345678901234567890;
 
-console.log(bigNumber);
-console.log(myInt);
+console.log(bigNumber); // Output: 1234567890123456789012345678901234567890n
+console.log(myInt); // Output: 1.2345678901234568e+39
 
-/* output
-1234567890123456789012345678901234567890n
-1.2345678901234568e+39
+/*
+	Meskipun digunakan untuk menyimpan angka dengan nilai besar, namun BigInt tetap
+	bisa digunakan untuk nilai yang lebih kecil.
 */
+
+const bigIntButSmall = 7n;
+
+/*
+	Kita juga bisa menggunakan BigInt untuk operasi aritmatika pada umumnya. Yang
+	membedakan adalah pada operasi pembagian, hasilnya akan dibulatkan ke bawah dan
+	tanpa mengandung nilai desimal, Contohnya adalah seperti ini:
+*/
+
+// console.log(`5n + 2n: ${5n + 2n}`); // Output: 7n
+// console.log(`5n - 2n: ${5n - 2n}`); // Output: 3n
+// console.log(`5n * 2n: ${5n * 2n}`); // Output: 10n
+// console.log(`5n / 2n: ${5n / 2n}`); // Output: 2n (Bukan 2.5n)
+// console.log(`5n % 2n: ${5n % 2n}`); // Output: 1n
+
+console.log(5n + 2n);
+console.log(5n - 2n);
+console.log(5n * 2n);
+console.log(5n / 2n);
+console.log(5n % 2n);
---
+++ back-end/Belajar Dasar Pemrograman JavaScript/Tipe Data/tmp.js
@@ -0,0 +1,8 @@
+console.log(`${5n + 2n}`);
+console.log(5n + 2n);
+
+const x = `${5n + 2n}`;
+const y = 5n + 2n;
+
+console.log(typeof x);
+console.log(typeof y);
