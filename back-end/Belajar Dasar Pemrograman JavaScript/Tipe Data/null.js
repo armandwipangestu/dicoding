f4e767d7a4bb581d84d3b0279ebe30130b164afd
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-06-15 09:02:18 +0700

    JavaScript: Tipe Data -> null

---
+++ back-end/Belajar Dasar Pemrograman JavaScript/Tipe Data/null.js
@@ -0,0 +1,15 @@
+/*
+Null:
+	Tipe berikutnya adalah null. Serupa dengan undefined, namun null perlu diinisialisasikan pada
+	variable. Null biasa digunakan sebagai nilai sementara pada variable, tapi sebenarnya nilai tersebut
+	"tidak ada".
+	
+	Terkadang kita perlu membuat sebuah variable, namun kita belum memerlukan nilai apa - apa dan tidak
+	ingin terikat oleh tipe data apa pun. Nah, daripada kita tidak menetapkan nilai apa pun (variable akan undefined)
+	sebaiknya kita beri nilai null pada variable tersebut dan ubah nanti ketika kita membutuhkannya.
+
+	Untuk menetapkan null pada variable, kita dapat gunakan keyword null ketika variable tersebut diinisialisasi.
+*/
+
+let someLaterData = null;
+console.log(someLaterData); // Output: null
